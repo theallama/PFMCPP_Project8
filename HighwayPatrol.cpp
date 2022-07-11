8e602ae72d69f9eca5aa667ad714d73141d35c38
theallama <>
2022-07-11 15:59:59 +0000

    use getVehicleType()

--- HighwayPatrol.cpp
+++ HighwayPatrol.cpp
@@ -35,21 +35,8 @@ void HighwayPatrol::pullOver( Vehicle* v, bool willArrest, Highway* h )
     std::cout << name << ": vehicle is traveling " << v->speed - h->speedLimit << " miles per hour over the speed limit" << std::endl;
     if( willArrest )
     {
-        std::string vehicleType;
-        
-        if(auto* car = dynamic_cast<Car*>(v))
-        {
-            vehicleType = "car";
-        }
-        else if(auto* mCycle = dynamic_cast<Motorcycle*>(v))
-        {
-            vehicleType = "motorcycle";
-        }
-        else if(auto* truck = dynamic_cast<SemiTruck*>(v))
-        {
-            vehicleType = "truck";
-        }
-        
+        auto vehicleType = getVehicleType(v);
+    
         //assert(false);
         //print the vehicle type in this std::cout between "THE [" and "] PULL". 
         std::cout << name << ": YOU IN THE [ " << vehicleType << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
--- HighwayPatrol.h
+++ HighwayPatrol.h
@@ -2,6 +2,11 @@
 
 #include "Vehicle.h"
 
+#include "Car.h"
+#include "Motorcycle.h"
+#include "SemiTruck.h"
+
+
 struct Highway;
 
 struct HighwayPatrol : Vehicle 
@@ -13,4 +18,21 @@ struct HighwayPatrol : Vehicle
     void pullOver( Vehicle* v, bool willArrest, Highway* h );
 
     void setSpeed(int s) override;
+
+    std::string getVehicleType(Vehicle* v)
+    {
+    if(auto* car = dynamic_cast<Car*>(v))
+    {
+        return("car");
+    }
+    else if(auto* mCycle = dynamic_cast<Motorcycle*>(v))
+    {
+        return("motorcycle");
+    }
+    else if (auto* truck = dynamic_cast<SemiTruck*>(v))
+    {
+        return("truck");
+    }
+    return ("N/A");
+}
 };
